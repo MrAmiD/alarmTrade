491595fedbca1746cd8bf4ad51ba1d6a9d667564
MrAmiD <nadymov2910@gmail.com>
2018-06-09 16:54:51 +0500

    Оформление заказа законченно, сталось доделать правки написанные в todo

--- app/js/vue/modelorder.js
+++ app/js/vue/modelorder.js
@@ -2,6 +2,12 @@
  * Created by MrAmiD
  */
 
+/*
+* Для бэкенда
+* 1) Если orderInfo.city != 'Пермь', то доставка транспортной компанией orderInfo.transportCompanyInfo
+* 2) У #order-btn в параметре data-order находится номер заказа.
+* 3)
+* */
 
 $(function() {
     var app = new Vue({
@@ -12,8 +18,8 @@ $(function() {
                 clientType: 'fizik',//fizik or urik
                 email: '',//email клиента
                 phone: '',//телефон клиента
-                city: 'Пермь',//Город клиента
-                deliveryType: 'Доставка курьером',//тип доставки(самовывоз/ транспортная компания/ ...)
+                city: 'Екатеринбург',//Город клиента
+                deliveryType: '',//тип доставки('Самовывоз', 'Доставка курьером')
                 files: [],//прикрепленные файлы
                 deliveryAdress: { //адрес доставки
                     street: '',//улица
@@ -21,15 +27,32 @@ $(function() {
                     house: '',//дом
                     apartament: ''//квартира
                 },
+                transportCompanyInfo: {
+                    address: '', // адрес самовывоза
+                    companyName: '' // Название транспортной компании
+                },
+                addressCompany: 'г.Пермь, ул. Мира 86 офис 497',
                 deliveryDate: '',
-                commentClient: ''
+                commentClient: '',
+                paymentType: 'Картой' // Способ оплаты,
             },
-            stepType: 'deliveryInfo'
+            stepType: 'contactInfo'
         }, 
         computed: {
 
         },
         methods: {
+            setPaymentType: function (value) {
+                console.log('sss');
+                this.orderInfo.paymentType = value;
+            },
+            getActiveStep: function (value) {
+                if(value == this.stepType){
+                    return 'active';
+                }else {
+                    return '';
+                }
+            },
             changeSteps: function () {
                 $('.basket-order-i .tab-c button').removeClass('active');
                 $('.steps-c .step-i').removeClass('active');
@@ -38,24 +61,105 @@ $(function() {
             },
             changeDelivery: function (selectedCity) {
                 this.orderInfo.city = selectedCity;
+
+                if(selectedCity.toUpperCase() != 'Пермь'.toUpperCase()){
+                    this.orderInfo.deliveryType = 'Доставка курьером';
+                }
+
+                if(selectedCity.toUpperCase() == 'Пермь'.toUpperCase() && this.orderInfo.deliveryType == 'Доставка курьером'){
+                    $('#taxDel').click()
+                }
+
             },
             changeModel: function (elem) {
-                this.orderInfo[elem.data().model] = elem.val();
+                var splitArr = elem.data().model.split('.');
+                if(splitArr.length == 1){
+                    this.orderInfo[elem.data().model] = elem.val();
+                }else {
+                    if(splitArr[splitArr.length - 1] == 'companyName'){
+                        this.orderInfo.transportCompanyInfo.companyName = elem.val();
+                    }
+                }
             },
             previewFiles: function() {
                 this.orderInfo.files = this.$refs.myFiles.files;
             },
-            changeStepType: function (value, event) {
-                this.stepType = value;
-                $('.basket-order-i .tab-c button').removeClass('active');
-                $('.steps-c .step-i').removeClass('active');
-                $('.steps-c .step-i').eq($(this).index()).addClass('active');
-                if(event){
-                    $(event.currentTarget).addClass('active');
+            validTxtInput: function (selector) {
+                if($(selector).val().length >= 1){
+                    $(selector).removeClass('notValid');
+                    return true;
+                } else {
+                    $(selector).addClass('notValid');
+                    return false;
                 }
+            },
+            validPhoneInput: function (selector) {
+                if($(selector).val().length == 17){
+                    $(selector).removeClass('notValid');
+                    return true;
+                } else {
+                    $(selector).addClass('notValid');
+                    return false;
+                }
+            },
+
+             
+            changeStepType: function (value, event) {
+
+                // if(value != 'contactInfo'){
+                //     if(!this.validTxtInput('#clientName')
+                //         || !this.validPhoneInput('#clientPhone')
+                //         || !this.validTxtInput('#clientEmail')){
+                //         return;
+                //     }
+                // }
+                //
+                // if (value != 'deliveryInfo'){
+                //     if((!this.validTxtInput('#street')
+                //         || !this.validTxtInput('#lit')
+                //         || !this.validTxtInput('#house')
+                //         || !this.validTxtInput('#apartament'))
+                //         && (this.orderInfo.deliveryType == 'Доставка курьером')){
+                //         return;
+                //     }
+                // }
+
+                if(value == 'getOrder'){ // Оформление заказа
+                    $('.title-page').text('Ваш заказ № ' + $('#order-btn').data().order )
+                }else {
+                    $('.title-page').text('Оформление заказа')
+
+                }
+
+                this.stepType = value;
             },
             initStyler: function () {
                 $('.vue-input, .vue-select').styler({});
+            },
+            initCalendar: function () {//Календарь для выбора даты
+                var varthis = this;
+                var now = new Date();
+                var minDate = new Date(new Date().getTime() + 30 * 60 * 1000);//now +30 минут
+                var maxDate = new Date();
+                maxDate.setDate(maxDate.getDate()+30);//now + 30 day
+                var datetime = $('.datepicker-here').datepicker({
+                    dateFormat : 'dd.mm.yyyy',
+                    minDate: minDate,
+                    maxDate: maxDate,
+                    onSelect: function(fd, d, picker) {
+                        console.log('dateSelected');
+                        varthis.orderInfo.deliveryDate = $('.datepicker-here').val();
+
+                    }
+                });
+            }
+        },
+        watch: {
+            'orderInfo.transportCompanyInfo.companyName': function () {
+                var varthis = this;
+                setTimeout(function () {
+                    varthis.orderInfo.transportCompanyInfo.address =  $('.group-inpt.transport-c.active select').val();
+                }, 100);
             }
         },
         mounted: function () {
@@ -73,31 +177,28 @@ $(function() {
                 varthis.changeModel(elem);
             });
 
-            //Отслеживаем изменение города в блоке "Оформление заказа"
+            //Отслеживаем изменение select в блоке "Оформление заказа"
             varthis.changeDelivery($('#city-selectSteps').val());
             $('#city-selectSteps').change(function () {
                 var selectedCity = $(this).val().toUpperCase();
                 varthis.changeDelivery(selectedCity);
             });
 
-            //Календарь для выбора даты
-            var now = new Date();
-            var minDate = new Date(new Date().getTime() + 30 * 60 * 1000);//now +30 минут
-            var maxDate = new Date();
-            maxDate.setDate(maxDate.getDate()+30);//now + 30 day
-            var datetime = $('.datepicker-here').datepicker({
-                dateFormat : 'dd.mm.yyyy',
-                minDate: minDate,
-                maxDate: maxDate,
-                onSelect: function(fd, d, picker) {
-                    console.log('dateSelected');
-                    varthis.orderInfo.deliveryDate = $('.datepicker-here').val();
-
+            $('.transportAddress').change(function () {
+                if($(this).val().toUpperCase() != ''){
+                    varthis.orderInfo.transportCompanyInfo.address = $(this).val().toUpperCase();
                 }
             });
 
+            setTimeout(function () {
+                varthis.orderInfo.transportCompanyInfo.address =  $('.group-inpt.transport-c.active select').val();
+            }, 1000);
+
+            this.initCalendar();
+
             //Показываем vue после того как всё отрендерится
             $('#vueCont').css({"opacity": '1'});
-        }
+        },
+
     })
 });
